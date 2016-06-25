48d7984099b4324d9ac8f0f344919043acf655a6
Abdulrahman Hamdy <arahman.hamdy91@gmail.com>
2016-06-25 12:29:34 +0200

    remove the slider js from base layout

--- ecomm/static/js/main.js
+++ ecomm/static/js/main.js
@@ -1,10 +1,3 @@
 jQuery(document).ready(function($){
 	$(".memenu").memenu();
-	$("#slider").responsiveSlides({
-		auto: true,
-		nav: true,
-		speed: 500,
-		namespace: "callbacks",
-		pager: true
-    });
 });
