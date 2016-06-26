663e6f1cb9a09d53056314cf4ba0e647286416c6
Abdulrahman Hamdy <arahman.hamdy91@gmail.com>
2016-06-26 17:12:33 +0200

    Implement logout in registeration app

--- ecomm/registration/urls.py
+++ ecomm/registration/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
+import django.contrib.auth.urls
 from . import views
 
 urlpatterns = [
     url(r'^register$', views.register, name='register'),
     url(r'^register/done/$', views.RegisterDoneView.as_view(),name='register_done'),
-    url(r'', include('django.contrib.auth.urls')),
+    url(r'', include(django.contrib.auth.urls)),
 ]
