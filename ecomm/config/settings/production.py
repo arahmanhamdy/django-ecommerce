27870fe40a69f5853fa1918162fdd5452629e0a5
Abdulrahman Hamdy <arahman.hamdy91@gmail.com>
2016-07-02 01:00:44 +0200

    set url of odoo for production settings

--- ecomm/config/settings/production.py
+++ ecomm/config/settings/production.py
@@ -5,3 +5,6 @@ DEBUG = False
 ALLOWED_HOSTS = ['seechic.os34.tech']
 STATIC_ROOT = '/var/www/html/seechic.os34.tech/static/'
 MEDIA_ROOT = '/var/www/html/seechic.os34.tech/media/'
+
+# odoo integration
+ODOO_URL = "http://odoo.os34.tech"
